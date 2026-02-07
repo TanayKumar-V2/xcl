cc5cc7c389af859bc674f6ce708c9d4369ffc08c
TanayKumar-V2 <tanayk726@gmail.com>
2026-02-07 13:31:58 +0530

    Init native

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"];
-export const presets = [require("nativewind/preset")];
-export const theme = {
-  extend: {},
-};
-export const plugins = [];
+module.exports = {
+  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
