92dde44c58efe4aeb8acb765bedc172471c3bfeb
TanayKumar-V2 <tanayk726@gmail.com>
2026-02-07 23:34:45 +0530

    Added Auth

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,12 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
-import "../global.css"
+import { View, Text } from 'react-native'
+import React from 'react'
+import { useClerk } from '@clerk/clerk-expo'
+import { Button } from '@react-navigation/elements'
 
-export default function App() {
+const HomeScreen = () => {
+
+    const{signOut}=useClerk()
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
+    <View>
+      <Text>HomeScreen</Text>
+
+      <Button onPress={()=>signOut()} >Sign Out</Button>
     </View>
-  );
+  )
 }
+
+export default HomeScreen
