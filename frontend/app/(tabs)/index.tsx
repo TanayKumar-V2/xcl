dbb9618e92c70875488eaaa491ed7359cfa441ab
TanayKumar-V2 <tanayk726@gmail.com>
2026-02-13 00:03:03 +0530

    try

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,11 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { useUserSync } from '@/hooks/useUsersync'
+import SignOutButton from '@/components/SignOutButton'
 
 const HomeScreen = () => {
+
+    useUserSync()
+
   return (
     <SafeAreaView>
       <Text>HomeScreen</Text>
+      <SignOutButton/>
     </SafeAreaView>
   )
 }
--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,14 +1,19 @@
 import { Stack } from "expo-router";
 import { ClerkProvider } from '@clerk/clerk-expo'
-import {tokenCache} from '@clerk/clerk-expo/token-cache'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import '../global.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
-      <Stack screenOptions={{headerShown:false}}>
-        <Stack.Screen name="(auth)" options={{headerShown:false}}/>
+      <QueryClientProvider client={queryClient}>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         </Stack>
+      </QueryClientProvider>
     </ClerkProvider>
   )
 }
